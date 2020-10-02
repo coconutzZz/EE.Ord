3fc02fb75149b00148b953d825be53f85278fe8f
Uros Jures <ujures@lyoness.onmicrosoft.com>
2020-10-02 17:02:31 +0200

    prj structure, added blazor webassembly app

---
+++ EE.Ord.Main.App/Shared/WeatherForecast.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EE.Ord.Main.App.Shared
+{
+    public class WeatherForecast
+    {
+        public DateTime Date { get; set; }
+
+        public int TemperatureC { get; set; }
+
+        public string Summary { get; set; }
+
+        public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+    }
+}
---
+++ EE.Ord.Services/PatientsService.cs
@@ -0,0 +1,20 @@
+﻿using System.Threading.Tasks;
+using EE.Ord.Domain.MasterData;
+using EE.Ord.Shared.Database.Main;
+
+namespace EE.Ord.Services
+{
+    public class PatientsService : IPatientsService
+    {
+        private readonly IStorageBroker _storageBroker;
+
+        public PatientsService(IStorageBroker storageBroker)
+        {
+            _storageBroker = storageBroker;
+        }
+
+        public async ValueTask<Patient> AddNewPatient(Patient patient) => await _storageBroker.InsertPatientAsync(patient);
+
+
+    }
+}
