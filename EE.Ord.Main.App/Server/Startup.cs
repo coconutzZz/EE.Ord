820a2cbed3ab7df6eb092a81ab3ec760295c6634
Uros Jures <ujures@lyoness.onmicrosoft.com>
2020-10-02 17:05:12 +0200

    removed not needed files

--- EE.Ord.Main.App/Server/Startup.cs
+++ EE.Ord.Main.App/Server/Startup.cs
@@ -22,6 +22,7 @@ namespace EE.Ord.Main.App.Server
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddControllers();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
